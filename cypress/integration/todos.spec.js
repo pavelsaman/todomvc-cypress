892f45a66da0cbb6bba1d41ee5dae775c7a6c981
Pavel Saman <pavelsam@centrum.cz>
2021-05-22 12:02:02 +0200

    init commit

---
+++ cypress/integration/todos.spec.js
@@ -0,0 +1,246 @@
+/// <reference types="Cypress" />
+
+import {
+    allItems,
+    newTodos,
+    toggleTodo,
+    checkState,
+    toggleAll,
+    unToggleAll,
+    unToggleTodo, 
+    deleteTodo,
+    goToAll,
+    goToActive,
+    goToCompleted,
+    todosCount, 
+    clearCompleted,
+} from './utils';
+const ITEM_ONE = 'Learn Cypress';
+const ITEM_TWO = 'Learn JS';
+
+describe('Persistent state', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+        newTodos(ITEM_ONE, ITEM_TWO);
+    });
+
+    it('Keep todos after page reload', () => {
+        cy.reload();
+        checkState(ITEM_ONE, ITEM_TWO);
+    });
+});
+
+describe('Add new todos', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('Add two new todos', () => {
+        newTodos(ITEM_ONE)
+          .then(() => checkState(ITEM_ONE));
+    });
+
+    it('Add two new todos', () => {
+        newTodos(ITEM_ONE, ITEM_TWO)
+          .then(() => checkState(ITEM_ONE, ITEM_TWO));
+    });
+
+    it('Add two new todos of the same name', () => {
+        newTodos(ITEM_ONE, ITEM_ONE)
+          .then(() => checkState(ITEM_ONE, ITEM_ONE));
+    });
+
+    it('Add new todo on completed tab', () => {
+        newTodos(ITEM_ONE);
+        toggleTodo(0);
+        goToCompleted();
+        clearCompleted();
+        newTodos(ITEM_ONE)
+          .should('not.exist');
+        goToAll()
+          .then(() => checkState(ITEM_ONE));
+    });
+
+    it('Add new todo on active tab', () => {
+        newTodos(ITEM_ONE);
+        goToActive();
+        newTodos(ITEM_TWO)
+          .then(() => checkState(ITEM_ONE, ITEM_TWO));
+    });
+});
+
+describe('Toggle todos', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+        newTodos(ITEM_ONE, ITEM_TWO);
+    });
+
+    it('Toggle first todo', () => {
+        toggleTodo(0);
+    });
+
+    it('Toggle and untoggle first todo', () => {
+        toggleTodo(0);
+        unToggleTodo(0);
+    });
+
+    it('Toggle second todo', () => {
+        toggleTodo(1);
+    });
+
+    it('Toggle all todos', () => {
+        toggleAll();
+    });
+
+    it('Untoggle all todos', () => {
+        toggleAll();
+        unToggleAll();
+    });
+});
+
+describe('Delete todos', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+        newTodos(ITEM_ONE, ITEM_TWO);
+    });
+
+    it('Delete first todo', () => {
+        deleteTodo(0)
+          .then(() => checkState(ITEM_TWO));
+    });
+
+    it('Delete second todo', () => {
+        deleteTodo(1)
+          .then(() => checkState(ITEM_ONE));
+    });
+
+    it('Delete all todos', () => {
+        deleteTodo(0)
+        deleteTodo(0)
+          .then(() => allItems().should('not.exist'));
+    });
+});
+
+describe('See active todos', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+        newTodos(ITEM_ONE, ITEM_TWO);
+    });
+
+    it('See two active todos', () => {
+        goToActive()
+          .then(() => checkState(ITEM_ONE, ITEM_TWO));
+    });
+
+    it('See one active todo', () => {
+        toggleTodo(0);
+        goToActive()
+          .then(() => checkState(ITEM_TWO));
+    });
+
+    it('No active todos', () => {
+        toggleTodo(0);
+        toggleTodo(1);
+        goToActive()
+          .then(() => allItems().should('not.exist'));
+    });
+});
+
+describe('See completed todos', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+        newTodos(ITEM_ONE, ITEM_TWO);
+    });
+
+    it('See two completed todos', () => {
+        toggleTodo(0);
+        toggleTodo(1);
+        goToCompleted()
+          .then(() => checkState(ITEM_ONE, ITEM_TWO));
+    });
+
+    it('See one completed todo', () => {
+        toggleTodo(0);
+        goToCompleted()
+          .then(() => checkState(ITEM_ONE));
+    });
+
+    it('No completed todos', () => {
+        goToCompleted()
+          .then(() => allItems().should('not.exist'));
+    });
+});
+
+describe('Todo count', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('See count zero', () => {
+        todosCount()
+          .should('have.text', '0 items left');
+    });
+
+    it('See count one', () => {
+        newTodos(ITEM_ONE, ITEM_TWO);
+        todosCount()
+          .should('have.text', '2 items left');
+    });
+
+    it('See count one after completing one todo', () => {
+        newTodos(ITEM_ONE, ITEM_TWO);
+        toggleTodo(0);
+        todosCount()
+          .should('have.text', '1 item left');
+    });
+
+    it('See count one on active tab after completing one todo', () => {
+        newTodos(ITEM_ONE, ITEM_TWO);
+        toggleTodo(0);
+        goToActive();
+        todosCount()
+          .should('have.text', '1 item left');
+    });
+
+    it('See count one on completed tab after completing one todo', () => {
+        newTodos(ITEM_ONE, ITEM_TWO);
+        toggleTodo(0);
+        goToCompleted();
+        todosCount()
+          .should('have.text', '1 item left');
+    });
+
+    it('See count one after deleting one todo', () => {
+        newTodos(ITEM_ONE, ITEM_TWO);
+        deleteTodo(0);
+        todosCount()
+          .should('have.text', '1 item left');
+    });
+});
+
+describe('Clear completed todos', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+        newTodos(ITEM_ONE, ITEM_TWO);
+    });
+
+    it('Clear completed todos', () => {
+        toggleTodo(0);
+        goToCompleted();
+        clearCompleted();
+        allItems()
+          .should('not.exist');
+        goToActive()
+          .then(() => checkState(ITEM_TWO));
+        goToAll()
+          .then(() => checkState(ITEM_TWO));
+    });
+});
---
+++ cypress/integration/utils.js
@@ -0,0 +1,65 @@
+
+const ALL_ITEMS = '.todo-list li';
+const TOGGLE = '.toggle';
+const TOGGLE_ALL = '.toggle-all';
+
+export const allItems = () => cy.get(ALL_ITEMS);
+export const newTodos = (...todoNames) => {
+    todoNames.forEach(todoName => {
+        cy.get('.new-todo')
+          .type(`${todoName}{Enter}`);
+    });
+    return allItems();
+};
+export const toggleTodo = n => {
+    cy.get(TOGGLE)
+      .eq(n)
+      .check();
+
+    allItems()
+      .eq(n)
+      .should('have.class', 'completed');
+};
+export const unToggleTodo = n => {
+    cy.get(TOGGLE)
+      .eq(n)
+      .uncheck();
+
+    allItems()
+      .eq(n)
+      .should('not.have.class', 'completed');
+};
+export const toggleAll = () => {
+    cy.get(TOGGLE_ALL)
+      .check();
+
+    allItems()
+      .each(todo => {
+          cy.get(todo)
+            .should('have.class', 'completed');
+      });
+};
+export const unToggleAll = () => {
+    cy.get(TOGGLE_ALL)
+      .uncheck();
+
+    allItems()
+      .each(todo => {
+          cy.get(todo)
+            .should('not.have.class', 'completed');
+      });
+};
+export const checkState = (...todos) => {
+    allItems()
+      .should('have.length', todos.length)
+      .each((todo, i) => {
+         cy.get(todo)
+           .should('contain.text', todos[i]);
+      });
+};
+export const deleteTodo = n => cy.get('.destroy').invoke('show').eq(n).click();
+export const goToAll = () => cy.get('a').contains('All').click();
+export const goToActive = () => cy.get('a').contains('Active').click();
+export const goToCompleted = () => cy.get('a').contains('Completed').click();
+export const todosCount = () => cy.get('.todo-count');
+export const clearCompleted = () => cy.get('.clear-completed').click();
